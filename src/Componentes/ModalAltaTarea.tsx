215248537f80301a3a58df09d3bcf5b348352e87
Tomas Castellano <tomas@itaptec.com.ar>
2020-07-17 18:58:22 -0300

    Agrego modal para dar de alta tareas

---
+++ src/Componentes/ModalAltaTarea.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const ModalExample = (props: any) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [estaVisible, setEstaVisible] = useState(false);
+  const [colorBackground, setColorBackground] = useState('black');
+
+  const toggle = () => setEstaVisible(!estaVisible);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+
+      <Modal isOpen={estaVisible} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+
+        <ModalBody>
+            Alta tarea.
+        </ModalBody>
+
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default ModalExample;
