cbd90ce1f9c24fba731201ecb69b9a96557f1c52
NicolasWu96 <nicolaswu96@gmail.com>
2020-07-23 21:12:46 -0300

    Se le agrega una funcion nueva para obtener el numero de dia de la semana (0 al 6) siendo domingo 0 y sabado 6)

--- src/Util/UtilFechaHoy.ts
+++ src/Util/UtilFechaHoy.ts
@@ -10,6 +10,10 @@ class UtilFechaHoy {
         return this.obtenerFecha().toLocaleTimeString(['en-GB'], {hour: '2-digit', minute:'2-digit'})
     }
 
+    obtenerDiaDeSemana(){
+        return this.obtenerFecha().getDay();
+    }
+
 }
 
 export default new UtilFechaHoy();
